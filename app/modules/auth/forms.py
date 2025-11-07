23bb8bfdee02470aa2c1be0e2132149104ffa144
pabpergas <pabpergas@alum.us.es>
2025-11-07 18:13:17 +0100

    Add 2FA forms (setup and verification)

--- app/modules/auth/forms.py
+++ app/modules/auth/forms.py
@@ -16,3 +16,14 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember me")
     submit = SubmitField("Login")
+
+
+class TwoFactorSetupForm(FlaskForm):
+    token = StringField("Verification Code", validators=[DataRequired(), Length(min=6, max=6)])
+    submit = SubmitField("Enable 2FA")
+
+
+class TwoFactorVerifyForm(FlaskForm):
+    token = StringField("Authentication Code", validators=[DataRequired(), Length(min=6, max=8)])
+    use_backup_code = BooleanField("Use backup code")
+    submit = SubmitField("Verify")
