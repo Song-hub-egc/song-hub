1332a756824f23c84bb27746057af94e3cc60437
jesvii <jesvilfer@alum.us.es>
2025-12-08 22:56:49 +0100

    feat: Tests de carga para trendin_datasets
    Se han realizado las pruebas de carga, locust, para la actividad trendind_datasets

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,21 +1,20 @@
-from locust import HttpUser, TaskSet, task
+from locust import HttpUser, TaskSet, between, task
 
 from core.environment.host import get_host_for_locust_testing
 from core.locust.common import get_csrf_token
 
 
 class DatasetBehavior(TaskSet):
-    def on_start(self):
-        self.dataset()
-
     @task
     def dataset(self):
         response = self.client.get("/dataset/upload")
         get_csrf_token(response)
-
+    @task
+    def trending_home(self):
+        self.client.get("/", name="trending_home")
 
 class DatasetUser(HttpUser):
-    tasks = [DatasetBehavior]
-    min_wait = 5000
-    max_wait = 9000
+    wait_time = between(5, 9)
     host = get_host_for_locust_testing()
+    tasks = [DatasetBehavior]
+
